847027a87969310747a84a486e0a06a466a17174
Şehmus Emre DEMİRALAY <sehmusemre.demiralay@tuikmerkez.tuik.gov.tr>
2022-11-30 13:34:36 +0300

    request header eklendi

--- src/main/java/com/emre/demo/controller/Controller.java
+++ src/main/java/com/emre/demo/controller/Controller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,6 +54,11 @@ public class Controller {
 	@PostMapping("usersall")
 	public List<User> postUserAll(@RequestBody List<User> userlist) {
 		return userlist;
+	}
+	
+	@PostMapping("/header")
+	public String postHeader(@RequestHeader("my_header") String headerValue) {
+		return headerValue;
 	}
 	
 	
